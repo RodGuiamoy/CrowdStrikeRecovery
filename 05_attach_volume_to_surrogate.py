66140301783b7ddf5c2448be07ac54687cf89454
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-19 16:21:40 -0400

    ssm fix?

--- 05_attach_volume_to_surrogate.py
+++ 05_attach_volume_to_surrogate.py
@@ -25,27 +25,28 @@ def describe_volume(volume_id, session):
     except Exception as e:
         print(f'Function: describe_volume. Error message: {e}')
 
-def checkCommandStatus(curSession,commandId,instances):
-    inProgress = True
+def checkCommandStatus(commandId,instances):
+    
+    ssm_client = boto3.client('ssm',  region_name = region)
  
     #If instances is not a list, convert it to a list
  
-    if not isinstance(instances, list):
-        instances = [instances]
+    # if not isinstance(instances, list):
+    # instances = [instances]
 
     var = ""
     while not var:    
         ssmStatus = ""
         #print("Waiting 5 seconds before checking status...")
         time.sleep(10)
-        for ssmInstances in instances:
+        #for ssmInstances in instances:
             #print(ssmInstances)
-            output = curSession.get_command_invocation(
-                CommandId=commandId,
-                InstanceId=ssmInstances
-            )
-            #ssmStatus.append(str(output['StatusDetails']))
-            ssmStatus = str(output['StatusDetails'])
+        output = ssm_client.get_command_invocation(
+            CommandId=commandId,
+            InstanceId=instances
+        )
+        #ssmStatus.append(str(output['StatusDetails']))
+        ssmStatus = str(output['Status'])
 
             #print("Command ID: " + str(output['CommandId']) + " Instance: " + \
                 #str(output['InstanceId']) + " Status: " + str(output['StatusDetails']))
@@ -74,6 +75,6 @@ ssm_client = boto3.client('ssm',  region_name = region)
 ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
 ssm_run_response = ssm_client.send_command(InstanceIds = [instance_id], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120)
 command_id = ssm_run_response['Command']['CommandId']
-checkCommandStatus(session,command_id,instance_id)
+checkCommandStatus(command_id,instance_id)
 
 ssm_delete_response = ssm_client.delete_document(Name=ssm_doc_name)
