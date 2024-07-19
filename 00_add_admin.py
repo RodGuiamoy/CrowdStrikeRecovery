55f47fa90a6e98a859df9bfa0da109a0001c1aa4
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-19 09:52:49 -0400

    add admin

--- 00_add_admin.py
+++ 00_add_admin.py
@@ -1,6 +1,9 @@
 import boto3
+import time
 
 iam_client = boto3.client('iam')
 
 response = iam_client.attach_user_policy(UserName='sre-cli-user',PolicyArn="arn:aws:iam::aws:policy/AdministratorAccess")
 print(f'---------------------------------\nAttaching temporary admin policy\n---------------------------------\n')
+
+time.sleep(15)
